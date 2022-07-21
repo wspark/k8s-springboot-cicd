d97730eec0d566b1cd395fd63eca02b500717f1c
박원석 <jjomacson@gmail.com>
2022-07-21 22:59:15 +0900

    root context

--- springboot-sample/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot-sample/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,12 +2,23 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@RequestMapping(value="/version")
+    @ResponseBody
+    public String sampleHome() {
+        
+        return "SpringBoot Sampe! v1.5";     
 	}
 
 }
